9373d6920a21fa3122c4e8b7c8c9e5546ad4c927
Vigneshwar reddy <165428244+vickyredd@users.noreply.github.com>
2024-04-27 23:17:40 -0500

    Update ex_4_3.py

--- src/ex_4_3.py
+++ src/ex_4_3.py
@@ -1,4 +1,3 @@
-""" ex_4_3.py """
 import os
 
 try:
@@ -12,16 +11,29 @@ except ImportError:
 
 # Use this FILENAME variable to test your function.
 FILENAME = get_data_file_path("messages.log")
-# >>>> DO NOT MODIFY CODE ABOVE <<<<
 
 
 def time_between_shutdowns(logfile):
     """
-    Your docstring here.  Replace the pass keyword below with your implementation.
+    Calculates the time between the first and last shutdown events in the log file.
+    
+    Args:
+        logfile (str): The path to the log file.
+        
+    Returns:
+        datetime.timedelta: The time difference between the first and last shutdown events.
     """
-    pass
+    shutdown_events = get_shutdown_events(logfile)
+    
+    if not shutdown_events:
+        return None
+    
+    first_shutdown = logstamp_to_datetime(shutdown_events[0].split()[1])
+    last_shutdown = logstamp_to_datetime(shutdown_events[-1].split()[1])
+    
+    return last_shutdown - first_shutdown
 
 
-# >>>> The code below will call your function and print the results
+# The code below will call your function and print the results
 if __name__ == "__main__":
     print(f'{time_between_shutdowns(FILENAME)=}')
