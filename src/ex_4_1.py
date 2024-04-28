54fae83590e6b1f33b3ab11ca4e8175ae7dc1c66
Vigneshwar reddy <165428244+vickyredd@users.noreply.github.com>
2024-04-27 23:09:01 -0500

    Update ex_4_1.py

--- src/ex_4_1.py
+++ src/ex_4_1.py
@@ -1,4 +1,3 @@
-""" ex_4_1.py """
 import os
 
 try:
@@ -10,16 +9,22 @@ except ImportError:
 
 # Use this FILENAME variable to test your function.
 FILENAME = get_data_file_path('messages.log')
-# >>>> DO NOT MODIFY CODE ABOVE <<<<
 
 
 def num_shutdowns(logfile):
     """
-    Your docstring here.  Replace the pass keyword below with your implementation.
+    Counts the number of shutdown events in the log file.
+    
+    Args:
+        logfile (str): The path to the log file.
+        
+    Returns:
+        int: The number of shutdown events in the log file.
     """
-    pass
+    shutdown_events = get_shutdown_events(logfile)
+    return len(shutdown_events) // 2  # Each shutdown event has two entries
 
 
-# >>>> The code below will call your function and print the results
+# The code below will call your function and print the results
 if __name__ == "__main__":
     print(f'{num_shutdowns(FILENAME)=}')
