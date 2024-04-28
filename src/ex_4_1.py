724c2cdb024ad5e5300891c4f46ab9e4439a55f0
vickyredd <165428244+vickyredd@users.noreply.github.com>
2024-04-27 23:31:55 -0500

    Update ex_4_1.py

--- src/ex_4_1.py
+++ src/ex_4_1.py
@@ -31,3 +31,9 @@ def num_shutdowns(logfile):
 if __name__ == "__main__":
     print(f'{num_shutdowns(FILENAME)=}')
     
+
+
+# The code below will call your function and print the results
+if __name__ == "__main__":
+    print(f'{num_shutdowns(FILENAME)=}')
+    
