bc76491d40a957f1d4e8b3bafdb99dc60e9e1205
Vigneshwar reddy <165428244+vickyredd@users.noreply.github.com>
2024-04-27 23:16:20 -0500

    Update ex_4_2.py

--- src/ex_4_2.py
+++ src/ex_4_2.py
@@ -1,15 +1,20 @@
 """ ex_4_2.py """
 from datetime import datetime
 
-
 def logstamp_to_datetime(datestr):
     """
-    Your docstring here.  Replace the pass keyword below with your implementation.
+    Converts a log timestamp string to a datetime.datetime object.
+    
+    Args:
+        datestr (str): The log timestamp string in the format "YYYY-MM-DDTHH:MM:SS".
+        
+    Returns:
+        datetime.datetime: The datetime object corresponding to the input timestamp.
     """
-    pass
+    return datetime.strptime(datestr, '%Y-%m-%dT%H:%M:%S')
 
 
-# >>>> The code below will call your function and print the results
+# The code below will call your function and print the results
 if __name__ == "__main__":
     test_date = '2022-12-01T01:02:03'
     print(f'{logstamp_to_datetime(test_date)=}')
